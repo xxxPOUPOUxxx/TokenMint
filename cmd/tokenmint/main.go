36670ae75544ad595a948c6e3fae05beee4f1a23
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 21:07:21 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tokenmint/main.go
@@ -0,0 +1,23 @@
+// cmd/tokenmint/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tokenmint/internal/tokenmint"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tokenmint.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
